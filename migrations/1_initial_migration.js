3ce086096671cac9cc816453266bd0734bb912bc
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-06-15 17:06:16 +0200

    added test migration script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,18 @@
 const Migrations = artifacts.require("Migrations");
+const SafeMath = artifacts.require("SafeMath");
+const IterableAddressMapping = artifacts.require("IterableAddressMapping");
+const CrowdfundingCampaign = artifacts.require("CrowdfundingCampaign");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(SafeMath);
+  deployer.deploy(IterableAddressMapping);
+  deployer.link(IterableAddressMapping, CrowdfundingCampaign);
+  deployer.link(SafeMath,CrowdfundingCampaign)
+  deployer.deploy(
+    CrowdfundingCampaign,
+    ["0x21f31D83234ba226D5983920bfA55Ee637F49fDB","0x5b78581Be5D6bE6B46e530ED8df9454da9e57C4e"],
+    ["0xb183b7AD4d85796bE75236530FDad0890c89a711","0x5Ded608621466D150a0BFE31F826B97bFc40cE2E","0x0ce75a7180b27E8A3b0694CD3e0b838Bcd6C3B64"],
+    60*60*1
+  );
 };
