761e6819467f3c836806190bc4a417f2f8524018
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-06-19 17:21:24 +0200

    Implemented RQ-HIST-FE

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,7 @@ App = {
     fair_donation:true,
     state:0,
     is_expired:false,
+    is_withdrawable:false,
      
     init: function() { return App.initWeb3(); },
 
@@ -83,7 +84,7 @@ App = {
                 // If event has parameters: event.returnValues.*paramName*
             });  
             instance.withdrawn().on('data', function (event) { 
-                App.notify(event);
+                App.notify("Beneficiary "+event.returnValues["_from"]+" has withdrawn: "+ parseFloat(event.returnValues["_amount"])/1000000000000000000+ "ETH");
                 console.log("Event catched"); 
                 console.log(event);
                 // If event has parameters: event.returnValues.*paramName*
@@ -92,7 +93,7 @@ App = {
         return App.render(); 
     },
 
-   render: function() { 
+    render: function() { 
         // Retrieve contract instance
         App.contracts["CrowdfundingCampaign"].deployed().then(async(instance) =>{
             //Set campaing state
@@ -103,37 +104,6 @@ App = {
                 if(v==2n) LABEL="DONATION"
                 $("#state").attr("placeholder",LABEL);
             }); 
-            //Set campaign timer
-            instance.campaignCloses().then(async(sec) => {
-                var countDownDate = new Date(parseInt(sec)*1000).getTime();
-
-                // Update the count down every 1 second
-                var countdown = setInterval(function() {
-
-                    // Get today's date and time
-                    var now = new Date().getTime();
-                
-                    // Find the distance between now and the count down date
-                    var distance = countDownDate - now;
-
-                    // Time calculations for days, hours, minutes and seconds
-                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-                    // Display the result in the element with id="demo"
-                    $("#expire-date").attr("placeholder",days + "d " + hours + "h "
-                    + minutes + "m " + seconds + "s ");
-                
-                    // If the count down is finished, write some text
-                    if (distance < 0) {
-                        clearInterval(countdown);
-                        $("#expire-date").attr("placeholder","EXPIRED");
-                        App.is_expired=true;
-                    }
-                }, 1000);
-            })
             //organizer status list
             instance.organizers_list().then(async(result) => {
                 for(i=0; i<result[0].length; i++){
@@ -143,22 +113,106 @@ App = {
                     $("#orga_list").append("<li class='list-group-item'>"+result[0][i].toString()+" "+badge+"</li>")
                 }
             })
-            //beneficiaries status list
-            instance.donation_status().then(async(result) => {
-                this.benef_size=result[0].length;
+            //Get contract expire-date and finish the render
+            instance.campaignCloses().then(async(sec) => {
+                var countDownDate = new Date(parseInt(sec)*1000).getTime();
+                // Update the count down every 1 second
+                var countdown = countDownInterval(countDownDate,$("#expire-date"),() => {App.is_expired=true},countdown)
+                
+                //after contract information ready, render the final part of the page
+                if(window.location.pathname=="/contribute.html"){
+                    App.renderContribute(instance);
+                }
+                if(window.location.pathname=="/withdraw.html"){
+                    App.renderWithdraw(instance,countDownDate);
+                }
+            })      
+        });
+    },
+
+    renderContribute : function(instance){
+        //beneficiaries status list
+        instance.donation_status().then(async(result) => {
+            this.benef_size=result[0].length;
+            for(i=0; i<result[0].length; i++){
+                donation = parseFloat(BigInt(result[1][i]).toString())/1000000000000000000;
+                elem = "<tr>"+
+                    "<th scope='row'>"+i+"</th>"+
+                    "<td>"+result[0][i].toString()+"</td>"+
+                    "<td>"+donation+"</td>"+
+                    '<td><input type="text" value="0.0" id="donationbox-'+i+'" class="form-control" aria-label="Amount" oninput="donation_calculator('+i+')"" ></td>'+
+                "</tr>"
+                $("#table_benef").append(elem);
+            }
+            donation_type_toggle('fair');
+        })
+        //donation list history
+        instance.get_my_donations({from:App.account}).then(async(result) => {
+            if(result[0].length>0){
+                $('#contrib_history').append("<h4>Your contribution history:</h4>");
+                $('#contrib_history').append('<table class="table table-hover table-responsive" >'+
+                '<thead><tr>'+
+                    '<th scope="col">#</th>'+
+                    '<th scope="col">Address</th>'+
+                    '<th scope="col">Donated ETH</th>'+
+                  '</tr></thead>'+
+                '<tbody id="table_contrib">'+
+                '</tbody>'+
+                '</table>');
                 for(i=0; i<result[0].length; i++){
                     donation = parseFloat(BigInt(result[1][i]).toString())/1000000000000000000;
                     elem = "<tr>"+
                         "<th scope='row'>"+i+"</th>"+
                         "<td>"+result[0][i].toString()+"</td>"+
                         "<td>"+donation+"</td>"+
-                        '<td><input type="text" value="0.0" id="donationbox-'+i+'" class="form-control" aria-label="Amount" oninput="donation_calculator('+i+')"" ></td>'+
                     "</tr>"
-                    $("#table_benef").append(elem)
+                    $("#table_contrib").append(elem);
                 }
-                donation_type_toggle('fair');
-            })
-        });
+            }
+        })
+    },
+
+    renderWithdraw : function(instance,campaign_closes){
+        instance.donation_status({from:App.account}).then(async(result) => {
+            this.benef_size=result[0].length;
+            donation = 0;
+            found = false;
+            for(i=0; i<result[0].length; i++){
+                if(result[0][i].toLowerCase()==App.account.toString()){
+                    donation = parseFloat(BigInt(result[1][i]).toString())/1000000000000000000;
+                    found=true;
+                }
+            }
+            if(found){
+                var withdrawcountdown = countDownInterval(
+                    (campaign_closes+60*5*1000),
+                    $("#withdraw-btn"),
+                    () => {App.is_withdrawable=true},
+                    withdrawcountdown,
+                    "Withdraw now!",
+                    "text"
+                )
+                $('#earnings').text(donation+" ETH")
+                $("#withdraw-btn").click(function(){
+                    App.contracts["CrowdfundingCampaign"].deployed().then(async (instance) => {
+                        console.log(App.is_withdrawable)
+                        if(App.is_withdrawable && donation>0){
+                            instance.withdraw({from:App.account}).then(async() => {
+                                $('#successModal').modal(); 
+                            }).catch(async(err)=> { 
+                                console.log(err.stack);
+                                $('#error_trace').append(err.stack); 
+                                $('#transactionErrorModal').modal(); 
+                            });
+                        }else{
+                            alert("You can withdraw only if you have more than 0 ETH of donations and if the withdraw timer is expired")
+                        }
+                    });
+                })
+            }else{
+                $("#accountErrorModal").modal()
+            }      
+        })
     },
 
     donate: function(){
@@ -268,6 +322,34 @@ donation_type_toggle = function(type){
 
 function isInt(value) {
     return parseFloat(value)>=0.0 && !isNaN(value);
+}
+
+function countDownInterval(countDownDate,widget,expired_callback,countdown,expireMessage="EXPIRED",attr="placeholder") {
+    return setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+    
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result 
+        result = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        attr=='text'?widget.text(result):widget.attr(attr,result);
+    
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(countdown);
+            attr=='text'?widget.text(expireMessage):widget.attr(attr,expireMessage);
+            expired_callback();
+        }
+    }, 1000);
 }
 
 // Call init whenever the window loads
